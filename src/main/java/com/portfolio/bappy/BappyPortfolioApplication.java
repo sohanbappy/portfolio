9a90641aad70358a2a6efdfb0d393d628c4936b5
bappy <bappy@rokomari.com>
2020-11-04 02:07:30 +0600

    Project added

---
+++ src/main/java/com/portfolio/bappy/BappyPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.bappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BappyPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BappyPortfolioApplication.class, args);
+	}
+
+}
